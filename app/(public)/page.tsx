97e3fcfb3e687c98edbe583ee343f3000acf619f
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2026-08-14 05:21:29 +0600

    cta section added in the home page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,4 @@
+import { CtaSection } from "@/components/home/CtaSection";
 import { FeaturedSection } from "@/components/home/FeaturedSection";
 import { GenreSection } from "@/components/home/GenreSection";
 import { HeroSection } from "@/components/home/HeroSection";
@@ -12,6 +13,7 @@ export default function HomePage() {
       <TopRatedSection />
       <LatestSection />
       <GenreSection />
+      <CtaSection />
     </>
   );
 }
